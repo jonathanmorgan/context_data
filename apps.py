14c391b3c30db62dcaf326a22e73f99ef1bae63b
Jonathan Morgan <jonathan.morgan.007@gmail.com>
2022-01-19 20:58:38 -0500

    default_auto_field

--- apps.py
+++ apps.py
@@ -6,4 +6,8 @@ class ContextDataConfig(AppConfig):
 
     name = 'context_data'
 
+    default_auto_field = 'django.db.models.AutoField'
+    # if you have lots of rows:
+    # default_auto_field = 'django.db.models.BigAutoField'
+
 #-- END class ContextDataConfig --#
